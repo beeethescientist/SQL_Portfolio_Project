624ef07e0c99d17834093bf01ab58ffe51c5b0d2
beeethescientist <mphobereng4@icloud.com>
2026-05-16 18:16:37 +0900

    SQL portfolio project initial commit

---
+++ sql_load/3_modify_tables.sql
@@ -0,0 +1,22 @@
+
+-- NOTE: This has been updated from the video to fix issues with encoding
+COPY company_dim
+FROM '/Users/oarabilebereng/Desktop/Portfolio2026/SQL_Project_Data_Job_Analysis/csv_files/company_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_dim
+FROM '/Users/oarabilebereng/Desktop/Portfolio2026/SQL_Project_Data_Job_Analysis/csv_files/skills_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY job_postings_fact
+FROM '/Users/oarabilebereng/Desktop/Portfolio2026/SQL_Project_Data_Job_Analysis/csv_files/job_postings_fact.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+COPY skills_job_dim
+FROM '/Users/oarabilebereng/Desktop/Portfolio2026/SQL_Project_Data_Job_Analysis/csv_files/skills_job_dim.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
+
+SELECT *
+FROM company_dim
+LIMIT 100;
